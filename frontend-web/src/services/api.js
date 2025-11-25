c82c4392c687d0aa97a825de15f6f948bae54833
FabricioBortolucci <fabriciobrpc13@gmail.com>
2025-11-25 17:51:14 -0300

    Preparando para deploy: Dockerfile e Variáveis de Ambiente

--- frontend-web/src/services/api.js
+++ frontend-web/src/services/api.js
@@ -4,7 +4,7 @@ import { useRequestLogStore } from '../stores/requestLogStore'
 import { createPinia } from 'pinia'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api'
 });
 
 api.interceptors.request.use(config => {
